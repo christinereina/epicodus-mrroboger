86fe3f8e09dcc7fb0ffe019f990211435c47adb0
Christine Augustine <blacchoy@gmail.com>
2020-04-10 13:15:07 -0700

    add working for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,20 @@
 function robot(userInput) {
 
   var messages = ["Beep", "Boop", "Wont you be my neighbor"];
-
-  var numbers = userInput.toString().split("");
-  if (numbers.includes("3")) {
-    return messages[2];
-  } else if (numbers.includes("2")) {
-    return messages[1];
-  } else if (numbers.includes("1")) {
-    return messages[0];
-  } else 
-
   var list = [];
 
   for (var i = 1; i <= userInput; i++) {
-    if (i === 1) {
+    var numbers = i.toString();
+    if (numbers.includes("1")) {
       list.push(messages[0]);
-    } else if (i === 2) {
+    } else if (numbers.includes("2")) {
       list.push(messages[1]);
-    } else if (i === 3) {
+    } else if (numbers.includes("3")) {
       list.push(messages[2]);
     } else {
       list.push(i.toString());
     }
   }
-
   return list.join(", ");
 }
 
@@ -46,6 +36,24 @@ $(document).ready(function() {
 
   });
   $('#button').click(function(){
-    location.reload();
+    $("#form")[0].reset();
 });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
